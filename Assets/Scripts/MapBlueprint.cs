eb289f046a106ad8290440a799da92596c443d6a
Peter Witt <peter.witt.joergensen@gmail.com>
2017-11-07 15:12:19 +0100

    Added Scripts for map stuff

---
+++ Assets/Scripts/MapBlueprint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGeneration
+{
+    /// <summary>
+    /// Purpose:
+    /// Creator: Peter Witt
+    /// </summary>
+    public class MapBlueprint : ScriptableObject
+    {
+        [SerializeField]
+        private List<MapGenerationAlgorithm> _algorithmStack;
+
+        [SerializeField]
+        private Vector2Int _gridSize;
+
+        [SerializeField]
+        private Vector2Int _chunkSize;
+
+        [SerializeField]
+        private int _userSeed;
+
+        [SerializeField]
+        private bool _useWhiteList;
+
+        [SerializeField]
+        private List<Chunk> __whitelistedChunks; 
+
+        [SerializeField]
+        private bool _useBlackList;
+
+        [SerializeField]
+        private List<Chunk> __blacklistedChunks;
+
+        public Vector2Int GridSize { get { return _gridSize; } set { _gridSize = value; } }
+        public Vector2Int ChunkSize { get { return _chunkSize; } set { _chunkSize = value; } }
+        public int UserSeed { get { return _userSeed; } set { _userSeed = value; } }
+
+        /// <summary>
+        /// Generates a map from the blueprint
+        /// </summary>
+        /// <param name="map">map</param>
+        public void Generate(Map map)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MapBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGeneration
+{
+    public class MapBuilder : Singleton<MapBuilder>
+    {
+        [SerializeField] private MapBlueprint _currentBlueprint;
+
+        [SerializeField] private ResourceHandler _resourceHandler;
+
+        public Map ActiveMap { get; set; }
+        public List<Map> Maps { get; set; }
+
+        /// <summary>
+        /// Generates a map from a specific blueprint
+        /// </summary>
+        /// <param name="mapBlueprint">blueprint</param>
+        /// <returns>Map</returns>
+        public Map Generate(MapBlueprint mapBlueprint)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Generates a map form current blueprint
+        /// </summary>
+        /// <returns>Map</returns>
+        public Map Generate()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Saves a map
+        /// </summary>
+        /// <param name="map">map</param>
+        public void Save(Map map)
+        {
+
+        }
+
+        /// <summary>
+        /// Spawns a map as instances
+        /// </summary>
+        /// <param name="map">map</param>
+        public void Spawn(Map map)
+        {
+
+        }
+
+        /// <summary>
+        /// Despawns a map from the world
+        /// </summary>
+        /// <param name="map">map</param>
+        public void Despawn(Map map)
+        {
+            
+        }
+    }
+}
