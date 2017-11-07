0941457038e1527ae11c0f10bcd6e1276571c7bb
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-11-07 14:27:25 +0100

    Added decription for the Chunk class and moved it to the MapGeneration namespace.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -3,102 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public enum ChunkType
+namespace MapGeneration
 {
-    Default, DeadEnd, Reward, Secret
-}
-
-public class Chunk : MonoBehaviour
-{
-    //This sections is for generel properties 
-    [Header("Properties"),SerializeField]
-    private int _width;//Tells the width of the chunk
-
-    [SerializeField]
-    private int _height;//Tells the height of the chunk
-
-    [SerializeField]
-    private ChunkType _chunkType;//Tells the type of the chunk
-
-    //These fields tells what openings are open on the chunk
-    [Header("Openings"),SerializeField]
-    private bool _topOpen;
-
-    [SerializeField]
-    private bool _bottomOpen;
-
-    [SerializeField]
-    private bool _leftOpen;
-
-    [SerializeField]
-    private bool _rightOpen;
-
-    //This section if for refernces
-    [Header("Refernces"),SerializeField]
-    private ChunkBehavior _chunkBehavior;
-
-    [SerializeField]
-    private Tilemap _enviorment;
-
-    //Properties for generel properties
-    public int Width
+    public enum ChunkType
     {
-        get { return _width; }
-        set { _width = value; }
-    }
-    public int Height
-    {
-        get { return _height; }
-        set { _height = value; }
-    }
-    public ChunkType ChunkType
-    {
-        get { return _chunkType; }
-        set { _chunkType = value; }
+        Default, DeadEnd, Reward, Secret
     }
 
-    //Properties for opennings
-    public bool TopOpen
-    {
-        get { return _topOpen; }
-        set { _topOpen = value; }
-    }
-    public bool BottomOpen
-    {
-        get { return _bottomOpen; }
-        set { _bottomOpen = value; }
-    }
-    public bool LeftOpen
-    {
-        get { return _leftOpen; }
-        set { _leftOpen = value; }
-    }
-    public bool RightOpen
-    {
-        get { return _rightOpen; }
-        set { _rightOpen = value; }
-    }
 
-    //Properties for references
-    public ChunkBehavior ChunkBehavior
+    /// <summary>
+    /// Purpose:
+    /// To Store the chunk's data.
+    /// Creator:
+    /// Mikkel Nielsen
+    /// </summary>
+    public class Chunk : MonoBehaviour
     {
-        get { return _chunkBehavior; }
-        set { _chunkBehavior = value; }
+        //This sections is for generel properties 
+        [Header("Properties"), SerializeField]
+        private int _width;//Tells the width of the chunk
+
+        [SerializeField]
+        private int _height;//Tells the height of the chunk
+
+        [SerializeField]
+        private ChunkType _chunkType;//Tells the type of the chunk
+
+        //These fields tells what openings are open on the chunk
+        [Header("Openings"), SerializeField]
+        private bool _topOpen;
+
+        [SerializeField]
+        private bool _bottomOpen;
+
+        [SerializeField]
+        private bool _leftOpen;
+
+        [SerializeField]
+        private bool _rightOpen;
+
+        //This section if for refernces
+        [Header("Refernces"), SerializeField]
+        private ChunkBehavior _chunkBehavior;
+
+        [SerializeField]
+        private Tilemap _enviorment;
+
+        //Properties for generel properties
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public ChunkType ChunkType
+        {
+            get { return _chunkType; }
+            set { _chunkType = value; }
+        }
+
+        //Properties for opennings
+        public bool TopOpen
+        {
+            get { return _topOpen; }
+            set { _topOpen = value; }
+        }
+        public bool BottomOpen
+        {
+            get { return _bottomOpen; }
+            set { _bottomOpen = value; }
+        }
+        public bool LeftOpen
+        {
+            get { return _leftOpen; }
+            set { _leftOpen = value; }
+        }
+        public bool RightOpen
+        {
+            get { return _rightOpen; }
+            set { _rightOpen = value; }
+        }
+
+        //Properties for references
+        public ChunkBehavior ChunkBehavior
+        {
+            get { return _chunkBehavior; }
+            set { _chunkBehavior = value; }
+        }
+        public Tilemap Enviorment
+        {
+            get { return _enviorment; }
+            set { _enviorment = value; }
+        }
+
+        public string ID { get; set; }
+
+        //These properties tells te chunk whick openings are used
+        public bool TopConnection { get; set; }
+        public bool BottomConnetion { get; set; }
+        public bool LeftConnection { get; set; }
+        public bool RightConnection { get; set; }
+
+        //A list for the items in the chunk
+        public List<GameObject> Items { get; set; }
     }
-    public Tilemap Enviorment
-    {
-        get { return _enviorment; }
-        set { _enviorment = value; }
-    }
-
-    public string ID { get; set; }
-
-    //These properties tells te chunk whick openings are used
-    public bool TopConnection { get; set; }
-    public bool BottomConnetion { get; set; }
-    public bool LeftConnection { get; set; }
-    public bool RightConnection { get; set; }
-
-    //A list for the items in the chunk
-    public List<GameObject> Items { get; set; }
 }
