9f78cf021eda70b9b064d39319701d5b2bd3a057
NielsJustesen <niels449@hotmail.com>
2017-11-07 14:12:32 +0100

    ChunkBehavior added.

---
+++ Assets/Scripts/ChunkBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChunkBehavior
+{
+    /// <summary>
+    /// Purpose:
+    /// To add behavior to the chunks, such as close exits or more.
+    /// Creator:
+    /// Niels Justesen
+    /// </summary>
+    public class ChunkBehavior : MonoBehaviour 
+    {
+        [SerializeField]
+        private Chunk chunk;
+        public Chunk Chunk { get { return chunk; } }
+
+        private List<GameObject> exitFillers;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void CloseExits()
+        {
+
+        }
+
+        public void UpdateChunk()
+        {
+
+        }
+
+    }
+}
