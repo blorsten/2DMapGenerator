130b2a9e49e700e9ee79b748f94c367ef6a22d10
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-11-09 09:23:21 +0100

    Compile error fix.

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -38,27 +38,5 @@ namespace MapGeneration
                 }
             }
         }
-
-        public virtual void OnDrawGizmos()
-        {
-
-            for (int x = 0; x < MapBlueprint.GridSize.x; x++)
-            {
-                for (int y = 0; y < MapBlueprint.GridSize.y; y++)
-                {
-                    float xPosition = x;
-                    float yPosition = y;
-
-                    float xOffset = Size.x * (_cellGap.x + _cellSize.x) / 2 - _cellSize.x / 2;
-                    float yOffset = Size.y * (_cellGap.y + _cellSize.y) / 2 - _cellSize.y / 2;
-
-                    Vector2 point = new Vector2(
-                        transform.position.x + xPosition - xOffset,
-                        transform.position.y + yPosition - yOffset);
-
-                    Gizmos.DrawWireCube(point, _cellSize);
-                }
-            }
-        }
     }
 }
