3806bd7afa923993b688ae6a0538b1f4a1eba4b5
NielsJustesen <niels449@hotmail.com>
2017-11-13 14:36:41 +0100

    CreateChunk started on class

---
+++ Assets/Scripts/CreateChunk.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CreateChunk
+{
+    /// <summary>
+    /// Purpose:
+    /// Creator:
+    /// </summary>
+    public class CreateChunk : MonoBehaviour 
+    {
+    }
+}
