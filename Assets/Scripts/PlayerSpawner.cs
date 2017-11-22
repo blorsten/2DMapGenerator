ccf6ebbb29c160601ab299607d2175af21d02ff5
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-11-22 16:19:13 +0100

    Changes to some systems

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGeneration
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject __player;
+
+        public void Start()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player == null)
+                Instantiate(__player, transform.position, Quaternion.identity).transform.position = transform.position;
+            else
+            {
+                player.transform.position = transform.position;
+            }
+        }
+    }
+}
+
