4a7573fb12706349891e1966460ef5b0ef23f453
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-11-22 10:16:33 +0100

    Minor Tweak

--- Assets/Scripts/ChunkGizmos.cs
+++ Assets/Scripts/ChunkGizmos.cs
@@ -5,145 +5,153 @@ using MapGeneration;
 using MapGeneration.Extensions;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class ChunkGizmos : MonoBehaviour
+namespace MapGeneration.Gizmos
 {
-    [Header("Gizmos"), SerializeField]
-    private bool _showConnectionWhenPlay = true;
-    [SerializeField]
-    private bool _showBacktrackingWhenPlay = true;
-    [SerializeField]
-    private bool _showEdgesWhenPlay = true;
-    [SerializeField]
-    private GizmoIcons _gizmos;
-
-    private Chunk _chunk;
-
-    public Chunk Chunk
+    [ExecuteInEditMode]
+    public class ChunkGizmos : MonoBehaviour
     {
-        get
+
+        [Header("References"),SerializeField]private GizmoIcons _gizmos;
+
+        [Header("Draw Booleans"), SerializeField] private bool _drawConnections = true;
+        [SerializeField] private bool _drawBacktracking = true;
+        [SerializeField] private bool _drawEdges = true;
+        [SerializeField] private bool _drawTileFlags = true;
+        
+
+        private Chunk _chunk;
+
+        public Chunk Chunk
         {
-            if (!_chunk)
-                _chunk = GetComponent<Chunk>();
-            return _chunk;
-        }
-    }
-
-    public void Awake()
-    {
-        if(!_gizmos)
-            Debug.LogWarning(gameObject.name + " is missing a " + typeof(GizmoIcons).Name);
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (!Chunk)
-            return;
-
-        DrawBackTracking();
-        DrawConnections();
-        DrawEdges();
-        DrawFlags();
-    }
-
-    private void DrawFlags()
-    {
-        if (!_gizmos)
-            return;
-        foreach (var flag in Chunk.TileFlags)
-        {
-            switch (flag.Type)
+            get
             {
-                case TileType.Trap:
-                    Gizmos.DrawIcon(Chunk.Enviorment.GetCellCenterWorld(flag.Position),_gizmos.TrapIcon,true);
-                    break;
-                case TileType.Treasure:
-                    Gizmos.DrawIcon(Chunk.Enviorment.GetCellCenterWorld(flag.Position), _gizmos.TreasureIcon, true);
-                    break;
-                case TileType.FlyingSpawn:
-                    Gizmos.DrawIcon(Chunk.Enviorment.GetCellCenterWorld(flag.Position), _gizmos.FlyingIcon, true);
-                    break;
-                case TileType.GroundSpawn:
-                    Gizmos.DrawIcon(Chunk.Enviorment.GetCellCenterWorld(flag.Position), _gizmos.GroundIcon, true);
-                    break;
-
+                if (!_chunk)
+                    _chunk = GetComponent<Chunk>();
+                return _chunk;
             }
         }
-    }
 
-    private void DrawEdges()
-    {
-        if (!(!_showEdgesWhenPlay && Application.isPlaying) && Chunk.Enviorment)
+        public void Awake()
         {
-            Gizmos.color = Color.white;
-            Vector2 gridSize = new Vector2(Chunk.Width, Chunk.Height);
-            Vector2 cellSize = Chunk.Enviorment.cellSize;
-
-            float yMin = transform.position.y - gridSize.y * cellSize.y / 2;
-            float yMax = transform.position.y + gridSize.y * cellSize.y / 2;
-            float xMin = transform.position.x - gridSize.x * cellSize.x / 2;
-            float xMax = transform.position.x + gridSize.x * cellSize.x / 2;
-
-            Gizmos.DrawLine(new Vector3(xMin, yMin), new Vector3(xMin, yMax));
-            Gizmos.DrawLine(new Vector3(xMax, yMin), new Vector3(xMax, yMax));
-            Gizmos.DrawLine(new Vector3(xMin, yMin), new Vector3(xMax, yMin));
-            Gizmos.DrawLine(new Vector3(xMin, yMax), new Vector3(xMax, yMax));
+            if (!_gizmos)
+                Debug.LogWarning(gameObject.name + " is missing a " + typeof(GizmoIcons).Name);
         }
-    }
 
-    private void DrawConnections()
-    {
-        if (!(!_showConnectionWhenPlay && Application.isPlaying) && Chunk.Enviorment)
+        private void OnDrawGizmos()
         {
-            Gizmos.color = new Color(231f / 255f, 76f / 255f, 60f / 255f);
+            if (!Chunk)
+                return;
 
-            foreach (var c in Chunk.Connections)
+            DrawBackTracking();
+            DrawConnections();
+            DrawEdges();
+            DrawFlags();
+        }
+
+        private void DrawFlags()
+        {
+            if (!_gizmos || !_drawTileFlags)
+                return;
+            foreach (var flag in Chunk.TileFlags)
             {
-                Vector3 cellPosition = Chunk.Enviorment.GetCellCenterWorld(c.Position);
-                Vector2 cellSize = Chunk.Enviorment.cellSize;
+                Vector3 postition = Chunk.Enviorment.GetCellCenterWorld(flag.Position);
 
-                Vector3 top = new Vector3(cellPosition.x, cellPosition.y + cellSize.y / 2);
-                Vector3 bottom = new Vector3(cellPosition.x, cellPosition.y - cellSize.y / 2);
-                Vector3 right = new Vector3(cellPosition.x + cellSize.x / 2, cellPosition.y);
-                Vector3 left = new Vector3(cellPosition.x - cellSize.x / 2, cellPosition.y);
-
-                switch (c.Type)
+                switch (flag.Type)
                 {
-                    case TileType.Top:
-                        GizmoUtilities.DrawArrow(top, ArrowDirection.Up);
+                    case TileType.Trap:
+                        UnityEngine.Gizmos.DrawIcon(postition, _gizmos.TrapIcon, true);
                         break;
-                    case TileType.Bottom:
-                        GizmoUtilities.DrawArrow(bottom, ArrowDirection.Down);
+                    case TileType.Treasure:
+                        UnityEngine.Gizmos.DrawIcon(postition, _gizmos.TreasureIcon, true);
                         break;
-                    case TileType.Left:
-                        GizmoUtilities.DrawArrow(left, ArrowDirection.Left);
+                    case TileType.FlyingSpawn:
+                        UnityEngine.Gizmos.DrawIcon(postition, _gizmos.FlyingIcon, true);
                         break;
-                    case TileType.Right:
-                        GizmoUtilities.DrawArrow(right, ArrowDirection.Right);
+                    case TileType.GroundSpawn:
+                        UnityEngine.Gizmos.DrawIcon(postition, _gizmos.GroundIcon, true);
                         break;
+
                 }
             }
         }
-    }
 
-    private void DrawBackTracking()
-    {
-        if (Chunk.ChunkHolder != null && !(!_showBacktrackingWhenPlay && Application.isPlaying))
+        private void DrawEdges()
         {
-            Gizmos.color = Color.red;
+            if (_drawEdges && Chunk.Enviorment)
+            {
+                UnityEngine.Gizmos.color = Color.white;
+                Vector2 gridSize = new Vector2(Chunk.Width, Chunk.Height);
+                Vector2 cellSize = Chunk.Enviorment.cellSize;
 
-            if (Chunk.ChunkHolder.ChunkOpenings.TopConnection)
-                Gizmos.DrawLine(transform.position,
-                    transform.position + Vector3.up * (Chunk.Height / 2f));
-            if (Chunk.ChunkHolder.ChunkOpenings.BottomConnetion)
-                Gizmos.DrawLine(transform.position,
-                    transform.position + Vector3.down * (Chunk.Height / 2f));
-            if (Chunk.ChunkHolder.ChunkOpenings.RightConnection)
-                Gizmos.DrawLine(transform.position,
-                    transform.position + Vector3.right * (Chunk.Width / 2f));
-            if (Chunk.ChunkHolder.ChunkOpenings.LeftConnection)
-                Gizmos.DrawLine(transform.position,
-                    transform.position + Vector3.left * (Chunk.Width / 2f));
+                float yMin = transform.position.y - gridSize.y * cellSize.y / 2;
+                float yMax = transform.position.y + gridSize.y * cellSize.y / 2;
+                float xMin = transform.position.x - gridSize.x * cellSize.x / 2;
+                float xMax = transform.position.x + gridSize.x * cellSize.x / 2;
+
+                UnityEngine.Gizmos.DrawLine(new Vector3(xMin, yMin), new Vector3(xMin, yMax));
+                UnityEngine.Gizmos.DrawLine(new Vector3(xMax, yMin), new Vector3(xMax, yMax));
+                UnityEngine.Gizmos.DrawLine(new Vector3(xMin, yMin), new Vector3(xMax, yMin));
+                UnityEngine.Gizmos.DrawLine(new Vector3(xMin, yMax), new Vector3(xMax, yMax));
+            }
+        }
+
+        private void DrawConnections()
+        {
+            if (_drawConnections && Chunk.Enviorment)
+            {
+                UnityEngine.Gizmos.color = new Color(231f / 255f, 76f / 255f, 60f / 255f);
+
+                foreach (var c in Chunk.Connections)
+                {
+                    Vector3 cellPosition = Chunk.Enviorment.GetCellCenterWorld(c.Position);
+                    Vector2 cellSize = Chunk.Enviorment.cellSize;
+
+                    Vector3 top = new Vector3(cellPosition.x, cellPosition.y + cellSize.y / 2);
+                    Vector3 bottom = new Vector3(cellPosition.x, cellPosition.y - cellSize.y / 2);
+                    Vector3 right = new Vector3(cellPosition.x + cellSize.x / 2, cellPosition.y);
+                    Vector3 left = new Vector3(cellPosition.x - cellSize.x / 2, cellPosition.y);
+
+                    switch (c.Type)
+                    {
+                        case TileType.Top:
+                            GizmoUtilities.DrawArrow(top, ArrowDirection.Up);
+                            break;
+                        case TileType.Bottom:
+                            GizmoUtilities.DrawArrow(bottom, ArrowDirection.Down);
+                            break;
+                        case TileType.Left:
+                            GizmoUtilities.DrawArrow(left, ArrowDirection.Left);
+                            break;
+                        case TileType.Right:
+                            GizmoUtilities.DrawArrow(right, ArrowDirection.Right);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void DrawBackTracking()
+        {
+            if (Chunk.ChunkHolder != null && _drawBacktracking && Chunk.Enviorment)
+            {
+                UnityEngine.Gizmos.color = Color.red;
+
+                if (Chunk.ChunkHolder.ChunkOpenings.TopConnection)
+                    UnityEngine.Gizmos.DrawLine(transform.position,
+                        transform.position + Vector3.up * (Chunk.Height / 2f));
+                if (Chunk.ChunkHolder.ChunkOpenings.BottomConnetion)
+                    UnityEngine.Gizmos.DrawLine(transform.position,
+                        transform.position + Vector3.down * (Chunk.Height / 2f));
+                if (Chunk.ChunkHolder.ChunkOpenings.RightConnection)
+                    UnityEngine.Gizmos.DrawLine(transform.position,
+                        transform.position + Vector3.right * (Chunk.Width / 2f));
+                if (Chunk.ChunkHolder.ChunkOpenings.LeftConnection)
+                    UnityEngine.Gizmos.DrawLine(transform.position,
+                        transform.position + Vector3.left * (Chunk.Width / 2f));
+            }
         }
     }
+
 }
+
+
--- Assets/Scripts/GizmoIcons.cs
+++ Assets/Scripts/GizmoIcons.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "GizmoIcons", menuName = "MapGeneration/Gizmos/GizmoIcons")]
-public class GizmoIcons : ScriptableObject
+namespace MapGeneration.Gizmos
 {
-    [SerializeField] private string _trapIcon;
-    [SerializeField] private string _treasureIcon;
-    [SerializeField] private string _groundIcon;
-    [SerializeField] private string _flyingIcon;
+    [CreateAssetMenu(fileName = "GizmoIcons", menuName = "MapGeneration/Gizmos/GizmoIcons")]
+    public class GizmoIcons : ScriptableObject
+    {
+        [SerializeField] private string _trapIcon;
+        [SerializeField] private string _treasureIcon;
+        [SerializeField] private string _groundIcon;
+        [SerializeField] private string _flyingIcon;
 
-    public string TrapIcon{get { return _trapIcon; }}
-    public string TreasureIcon{get { return _treasureIcon; }}
-    public string GroundIcon{get { return _groundIcon; }}
-    public string FlyingIcon{get { return _flyingIcon; }}
+        public string TrapIcon{get { return _trapIcon; }}
+        public string TreasureIcon{get { return _treasureIcon; }}
+        public string GroundIcon{get { return _groundIcon; }}
+        public string FlyingIcon{get { return _flyingIcon; }}
+    }
 }
