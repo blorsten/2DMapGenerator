b839dcae46a6cb21527845c5da6988e3a40f1b33
NielsJustesen <niels449@hotmail.com>
2017-11-30 11:42:24 +0100

    Algorithm should work.

--- Assets/Scripts/Algorithms/VoronoiBiome.cs
+++ Assets/Scripts/Algorithms/VoronoiBiome.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
 
 namespace MapGeneration.Algorithm
 {
@@ -9,7 +10,7 @@ namespace MapGeneration.Algorithm
     /// Purpose: 
     /// Set biome on the generated map with diamond square
     /// Creator:
-    /// Niels Justesen
+    /// Niels Justesen og Mikkel Bruun
     /// </summary>
     [CreateAssetMenu(fileName = "New Voronoir Biome", menuName = "MapGeneration/Algorithms/Voronoi Biome")]
     public class VoronoiBiome : MapGenerationAlgorithm
@@ -19,6 +20,7 @@ namespace MapGeneration.Algorithm
         private int _width;
         private int _heigt;
 
+        
 
         public override bool Process(Map map, List<Chunk> usableChunks)
         {
@@ -26,31 +28,113 @@ namespace MapGeneration.Algorithm
             _heigt = map.Grid.GetLength(1) * map.MapBlueprint.ChunkSize.y;
             _noiseGrid = new float[_width, _heigt];
 
-            //Set the random points in the grid
-            for (int i = 0; i < _nrOfPoints; i++)
-            {
-                float noise = map.Random.Range(0f, 1f);
-                
-                _noiseGrid[map.Random.Range(0, _width-1), map.Random.Range(0, _heigt-1)] = noise;
-            }
-
-
             for (int x = 0; x < _width; x++)
             {
                 for (int y = 0; y < _heigt; y++)
                 {
-                    if (_noiseGrid[x,y] > 0)
-                    {
-                        SetNeighborsOfPoint(x, y, _noiseGrid[x, y]);
-                    }
+                    _noiseGrid[x, y] = 0;
                 }
             }
+
+            //Set the random points in the grid
+            for (int i = 0; i < _nrOfPoints; i++)
+            {
+                float noise = map.Random.Range(0.01f, 1f);
+
+                _noiseGrid[map.Random.Range(0, _width - 1), map.Random.Range(0, _heigt - 1)] = noise;
+            }
+
+            _noiseGrid = VornonoiPopulation(_noiseGrid);
+
             return base.Process(map, usableChunks);
         }
 
-        private void SetNeighborsOfPoint(int x, int y, float noise)
+        public override bool PostProcess(Map map, List<Chunk> usableChunks)
         {
-           
+            foreach (ChunkHolder chunk in map.Grid)
+            {
+                if (chunk.Instance != null)
+                {
+                    int width = chunk.Instance.Width;
+                    int height = chunk.Instance.Height;
+
+                    for (int x = 0; x < chunk.Instance.Width; x++)
+                    {
+                        for (int y = 0; y < chunk.Instance.Height; y++)
+                        {
+                            chunk.Instance.BiomeValues[x, y] = _noiseGrid[x + width * chunk.Position.x, y + height * chunk.Position.y];
+                        }
+                    }
+                }
+            }
+            return base.PostProcess(map, usableChunks);
         }
+
+        private float[,] VornonoiPopulation(float[,] map)
+        {
+            List<VornonoiData> _dataToTouch = new List<VornonoiData>();
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _heigt; y++)
+                {
+                    if (map[x, y] > 0)
+                    {
+                        bool valid = false;
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            if (valid)
+                                break;
+                            for (int j = -1; j < 1; j++)
+                            {
+                                Vector2Int pos = new Vector2Int(x + i, y + j);
+                                if ((i == 0 && j == 0) || pos.x < 0 || pos.y < 0 || pos.x >= _width || pos.y >= _heigt)
+                                    continue;
+                                if(map[pos.x,pos.y] == 0)
+                                {
+                                    valid = true;
+                                    _dataToTouch.Add(new VornonoiData(new Vector2Int(x, y), map[x, y]));
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (_dataToTouch.Count <= 0)
+                return map;
+
+            _dataToTouch.Sort((x, y) => x.Value.CompareTo(y.Value));
+
+            foreach (var data in _dataToTouch)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    for (int y = -1; y <= 1; y++)
+                    {
+                        Vector2Int pos = data.Position + new Vector2Int(x, y);
+                        if ((x == 0 && y == 0) || pos.x < 0 || pos.y < 0 || pos.x >= _width || pos.y >= _heigt)
+                            continue;
+                        if (map[pos.x, pos.y] == 0)
+                            map[pos.x, pos.y] = data.Value;
+                    }
+                }
+            }
+            _dataToTouch.Clear();
+            return VornonoiPopulation(map);
+        }
+    }
+
+}
+
+public struct VornonoiData
+{
+    public Vector2Int Position { get; set; }
+    public float Value { get; set; }
+
+    public VornonoiData(Vector2Int postion, float value)
+    {
+        Position = postion;
+        Value = value;
     }
 }
