64177fefd3e155c7224dfdab2eaf4b45b5e0e6f3
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2017-11-30 14:39:53 +0100

    Updated the blueprints with new algorithm references

--- Assets/Scripts/Editor/MapBuilderEditor.cs
+++ Assets/Scripts/Editor/MapBuilderEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using MapGeneration;
+using MapGeneration.Extensions;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
