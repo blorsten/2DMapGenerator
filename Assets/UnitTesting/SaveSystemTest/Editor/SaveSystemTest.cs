b147c59afccd5ec6b26dbfc12c671d8268dcde12
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2017-12-08 15:25:35 +0100

    Added more tests to save system

--- Assets/UnitTesting/SaveSystemTest/Editor/SaveSystemTest.cs
+++ Assets/UnitTesting/SaveSystemTest/Editor/SaveSystemTest.cs
@@ -1,6 +1,9 @@
-﻿using MapGeneration;
+﻿using System.Collections;
+using MapGeneration;
+using MapGeneration.SaveSystem;
 using NUnit.Framework;
 using UnityEditor;
+using UnityEngine.TestTools;
 
 [TestFixture(Author = "MP", Category = "SaveSystem")]
 public class SaveSystemTest
@@ -11,14 +14,56 @@ public class SaveSystemTest
             AssetDatabase.LoadAssetAtPath<MapBlueprint>("Assets/UnitTesting/SaveSystemTest/SaveSystemBPTest.asset");
     }
 
-	[Test]
-	public void Save()
+    [Test]
+    public void MapDataSaver_Construction_And_Initalization()
     {
-
-	}
-
-    public void Load()
-    {
+        Map newMap = MapBuilder.Instance.Generate();
         
+        Assert.AreEqual(newMap, newMap.MapDataSaver.Map);
+        Assert.AreEqual(newMap.ID, newMap.MapDataSaver.MapId);
+        Assert.AreEqual(newMap.Seed, newMap.MapDataSaver.MapSeed);
+        Assert.AreEqual(newMap.MapBlueprint, newMap.MapDataSaver.MapBlueprint);
+    }
+
+    [Test]
+	public void MapDataSaver_Save()
+    {
+        Map newMap = MapBuilder.Instance.Generate();
+
+        newMap.GetComponentInChildren<SaveSystemTestScript>().TestData = 1337;
+
+        newMap.MapDataSaver.SavePersistentData();
+
+        Assert.IsTrue(newMap.MapDataSaver.HasSavedData);
+    }
+
+    [Test]
+    public void MapDataSaver_Load_With_Manual()
+    {
+        MapDataSaver newMap = MapBuilder.Instance.Generate().MapDataSaver;
+
+        newMap.Map.GetComponentInChildren<SaveSystemTestScript>().TestData = 1337;
+
+        Assert.IsFalse(newMap.HasSavedData);
+
+        newMap.SavePersistentData();
+
+        Assert.IsTrue(newMap.HasSavedData);
+
+        newMap.LoadPersistentData();
+
+        Assert.AreEqual(1337, newMap.Map.GetComponentInChildren<SaveSystemTestScript>().TestData);
+    }
+
+    [Test]
+    public void MapDataSaver_Load_With_Generation()
+    {
+        MapDataSaver newMap = MapBuilder.Instance.Generate().MapDataSaver;
+
+        newMap.Map.GetComponentInChildren<SaveSystemTestScript>().TestData = 1337;
+
+        Map secondMap = MapBuilder.Instance.Generate(newMap);
+
+        Assert.AreEqual(1337, secondMap.GetComponentInChildren<SaveSystemTestScript>().TestData);
     }
 }
